596d6532aa5792b4423df50f5ed3dee2ff03ab62
Benjamin Boortz <benjamin.boortz@web.de>
2015-12-25 13:39:36 +0100

    test.sh modified /BB

--- env/images/nodejs-newsfeed-service/test.sh
+++ env/images/nodejs-newsfeed-service/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl --data "name=Spiegel.de&url=http://www.spiegel.de/schlagzeilen/tops/index.rss" http://localhost:8080/api/feeds
+curl --data "name=Spiegel.de index&url=http://www.spiegel.de/schlagzeilen/tops/index.rss" http://localhost:8080/api/feeds
+curl --data "name=zeit.de index&url=http://newsfeed.zeit.de/index" http://localhost:8080/api/feeds
+curl --data "name=fefe.de&url=https://blog.fefe.de/rss.xml" http://localhost:8080/api/feeds
 curl http://localhost:8080/api/feeds
